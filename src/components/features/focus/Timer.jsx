18ca1ff62d7cbbd3791883f6ee5e5e665ee7d994
Jimmy2202 <artrkmj214@gmail.com>
2026-06-25 15:35:52 -0300

    timer message

--- src/components/features/focus/Timer.jsx
+++ src/components/features/focus/Timer.jsx
@@ -2,7 +2,6 @@ import { useEffect, useRef, useState } from "react";
 import { AnimatePresence, motion } from "motion/react";
 
 function Timer() {
-  const [timer, setTimer] = useState(0);
   const [segundos1, setSegundos1] = useState(0);
   const [segundos2, setSegundos2] = useState(0);
   const [minutos1, setminutos1] = useState(0);
@@ -10,7 +9,6 @@ function Timer() {
   const [horas1, sethoras1] = useState(0);
   const [horas2, sethoras2] = useState(0);
   const [click, setClick] = useState(false);
-  const [show, setShow] = useState(false);
   const [showMsg1, setShowMsg1] = useState(false);
   const [showMsg2, setShowMsg2] = useState(false);
   const [showDiv, setShowDiv] = useState(true);
@@ -28,27 +26,16 @@ function Timer() {
   const intervalRef = useRef(null);
 
   useEffect(() => {
-    function aparecerTemporariamente() {
-      setShowMsg1(true);
+    setShowMsg1(true);
 
-      setTimeout(() => {
-        setShowMsg1(false);
-      }, 8000);
+    setTimeout(() => setShowMsg1(false), 7000);
+    setTimeout(() => setShowMsg2(true), 7600);
+    setTimeout(() => setShowMsg2(false), 14000);
+    setTimeout(() => setShowDiv(false), 14200);
 
-      setTimeout(() => {
-        setShowMsg2(true);
-      }, 8600);
-
-      setTimeout(() => {
-        setShowMsg2(false);
-      }, 15600);
-
-      setTimeout(() => {
-        setShowDiv(false);
-      }, 15800);
-    }
-
-    aparecerTemporariamente();
+    return () => {
+      clearTimeout();
+    };
   }, []);
 
   function pause() {
@@ -56,10 +43,11 @@ function Timer() {
   }
 
   function count() {
+    clearInterval(intervalRef.current);
+
     intervalRef.current = setInterval(() => {
       const tempo = tempoRef.current;
 
-      tempo.timerx = tempo.timerx + 1;
       tempo.segundo2 = tempo.segundo2 + 1;
 
       if (tempo.segundo2 === 10) {
@@ -102,40 +90,47 @@ function Timer() {
         {showDiv ? (
           <motion.div
             exit={{ opacity: 0 }}
-            transition={{ duration: 4 }}
-            className="absolute bg-black/[0.5] backdrop-blur-sm w-screen h-screen flex items-center justify-center text-center"
+            transition={{ duration: 3 }}
+            className="
+              fixed inset-0 z-50
+              flex items-center justify-center
+              bg-black/60 backdrop-blur-md
+              px-6 text-center text-white
+            "
           >
             <AnimatePresence>
-              {showMsg1 ? (
+              {showMsg1 && (
                 <motion.p
-                  initial={{ opacity: 0, scale: 0 }}
-                  animate={{ opacity: 1, scale: 4 }}
-                  exit={{ opacity: 0, scale: 0 }}
-                  transition={{ duration: 4 }}
-                  className="absolute text-white"
+                  initial={{ opacity: 0, scale: 0.7 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  exit={{ opacity: 0, scale: 0.7 }}
+                  transition={{ duration: 2 }}
+                  className="absolute max-w-[320px] text-2xl sm:max-w-[600px] sm:text-4xl font-viaoda text-red-100"
                   key="msg1"
                 >
                   Tome seu tempo...
                 </motion.p>
-              ) : null}
+              )}
             </AnimatePresence>
+
             <AnimatePresence>
-              {showMsg2 ? (
+              {showMsg2 && (
                 <motion.p
-                  initial={{ opacity: 0, scale: 0 }}
-                  animate={{ opacity: 1, scale: 4 }}
-                  exit={{ opacity: 0, scale: 0 }}
-                  transition={{ duration: 4 }}
-                  className="text-[10px] absolute text-white"
+                  initial={{ opacity: 0, scale: 0.7 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  exit={{ opacity: 0, scale: 0.7 }}
+                  transition={{ duration: 2 }}
+                  className="absolute max-w-[320px] text-2xl sm:max-w-[600px] sm:text-4xl font-viaoda text-red-100"
                   key="msg2"
                 >
                   Inicie o relógio e faça alguma atividade...
                 </motion.p>
-              ) : null}
+              )}
             </AnimatePresence>
           </motion.div>
         ) : null}
       </AnimatePresence>
+
       <AnimatePresence initial={false}>
         {click ? (
           <motion.img
@@ -150,26 +145,30 @@ function Timer() {
           />
         ) : null}
       </AnimatePresence>
+
       <p className="mt-14 text-[30px] text-white font-silkscreen">
         {horas1}
         {horas2}:{minutos1}
         {minutos2}:{segundos1}
         {segundos2}
       </p>
+
       <button
         onClick={() => {
           count();
           setClick(true);
+          setIsPaused(false);
         }}
         className="bg-red-300 text-black p-3 rounded-lg hover:bg-red-800 hover:text-white hover:scale-105 transition duration-300"
       >
         {isPaused ? "Continuar" : "Começar"}
       </button>
+
       <button
         className="bg-red-300 text-black p-3 rounded-lg hover:bg-red-800 hover:text-white hover:scale-105 transition duration-300"
         onClick={() => {
           pause();
-          isPaused ? null : setIsPaused(true);
+          setIsPaused(true);
         }}
       >
         Pausar
